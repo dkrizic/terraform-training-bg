3b114a984ab9a98dd52c31f12fe0e9a50151db43
Darko Krizic <darko@krizic.net>
2022-04-11 11:37:18 +0200

    Creating resource group

---
+++ terraform/main.tf
@@ -0,0 +1,5 @@
+locals {
+  subscription        = "5383c002-d638-4986-a0e9-74decda8b3f4"
+  location            = "westeurope"
+  resource_group_name = "DKrizic"
+}
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,8 +1,14 @@
 provider "random" {
 }
 
+provider "azurerm" {
+  features {}
+  subscription_id = local.subscription
+}
+
 terraform {
   required_providers {
-    random = "3.1.2"
+    random  = "3.1.2"
+    azurerm = "3.1.0"
   }
 }
